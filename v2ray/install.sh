e0b7a984a2b684a29c8769cf22184ab32ce62d82
nimaxnimax <avaroti.service@gmail.com>
2023-12-27 10:17:00 +0100

    done

--- v2ray/install.sh
+++ v2ray/install.sh
@@ -5,12 +5,14 @@ sleep $sleeptime
 sudo apt update -y
 sudo apt install iftop mtr -y
 sleep $sleeptime
-sudo fallocate -l 4G /swapfile
-ls -anp /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+if [ ! -e "/swapfile" ]; then
+    sudo fallocate -l 4G /swapfile
+    ls -anp /swapfile
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+    echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+fi
 sleep $sleeptime
 sudo apt update -y
 sudo apt-get install python3 -y
